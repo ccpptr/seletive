9e1eb5e271fbdfccf4765a149d21c2b1fe605162
ccpptr <ccppeter@protonmail.com>
2022-11-23 07:46:31 -0300

    realiza tarefas

--- vagas/views.py
+++ vagas/views.py
@@ -37,7 +37,8 @@ def nova_vaga(request):
 
 def vaga(request, id):
     vaga = get_object_or_404(Vagas, id=id)
-    return render(request, "vaga.html", {"vaga": vaga})
+    tarefas = Tarefa.objects.filter(vaga=vaga).filter(realizada=False)
+    return render(request, "vaga.html", {"vaga": vaga, "tarefas": tarefas})
 
 
 def nova_tarefa(request, id_vaga):
@@ -56,3 +57,19 @@ def nova_tarefa(request, id_vaga):
     except:
         messages.add_message(request, constants.ERROR, "Erro interno do sistema")
         return redirect(f"/vagas/vaga/{id_vaga}")
+
+
+def realizar_tarefa(request, id):
+    tarefas_list = Tarefa.objects.filter(id=id).filter(realizada=False)
+
+    if not tarefas_list.exists():
+        messages.add_message(request, constants.ERROR, "Erro interno do sistema!")
+        return redirect(f"/home/empresas/")
+
+    tarefa = tarefas_list.first()
+    tarefa.realizada = True
+    tarefa.save()
+    messages.add_message(
+        request, constants.SUCCESS, "Tarefa realizada com sucesso, parabéns!"
+    )
+    return redirect(f"/vagas/vaga/{tarefa.vaga.id}")
